b3cd49616af843d3c7a4ba34984fc85f366713b4
Epp Bernard (ETAS/EES-SQ) <Bernard.Epp@etas.com>
2022-06-20 18:35:57 +0200

    added simple react frontend UI custom Component, custom hook, useEffect, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const { search, animals } = useAnimalSearch();
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
+}
+
+// Dumb UI Component
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+// Custom Hook
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem("lastQuery", q);
+  };
+  return { search, animals };
 }
 
 export default App;
